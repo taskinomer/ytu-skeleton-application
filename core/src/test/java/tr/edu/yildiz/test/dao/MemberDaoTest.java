a3d95254fbf0aa4f7c90cc332f5779db768e9b15
otaskin <otaskin@ebay.com>
2015-03-15 19:18:15 +0200

    memberdao integration test

---
+++ core/src/test/java/tr/edu/yildiz/test/dao/MemberDaoTest.java
@@ -0,0 +1,41 @@
+package tr.edu.yildiz.test.dao;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import tr.edu.yildiz.dao.MemberDao;
+import tr.edu.yildiz.domain.Member;
+import tr.edu.yildiz.test.GenericTest;
+
+import java.util.List;
+
+public class MemberDaoTest extends GenericTest {
+
+    @Autowired
+    private MemberDao memberDao;
+
+    @Test
+    public void shouldFindAllMembers() {
+        List<Member> members = memberDao.findAll();
+        Assert.assertNotNull(members);
+    }
+
+    @Test
+    public void shouldSaveMember() {
+        memberDao.saveOrUpdate(populateDummyMember());
+
+        List<Member> members = memberDao.findAll();
+        Assert.assertTrue(members.size() > 0);
+    }
+
+
+    private Member populateDummyMember() {
+        Member member = new Member();
+
+        member.setName("dummy");
+        member.setSurname("user");
+
+        return member;
+    }
+
+}
