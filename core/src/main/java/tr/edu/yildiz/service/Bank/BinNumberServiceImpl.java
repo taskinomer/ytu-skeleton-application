bcb2a526610b8c011da086cbe8e89277931799f3
Bozyel <abozyel@lm-ist-00p0dr53.corp.ebay.com>
2016-07-26 16:00:00 +0300

    Bank servis&Bank Servis impl yazıldı

---
+++ core/src/main/java/tr/edu/yildiz/service/Bank/BinNumberServiceImpl.java
@@ -0,0 +1,7 @@
+package tr.edu.yildiz.service.Bank;
+
+/**
+ * Created by abozyel on 26/07/16.
+ */
+public class BinNumberServiceImpl {
+}
---
+++ webapp/src/main/java/tr.edu.yildiz/controller/Bank/BinNumberController.java
@@ -0,0 +1,7 @@
+package tr.edu.yildiz.controller.Bank;
+
+/**
+ * Created by abozyel on 26/07/16.
+ */
+public class BinNumberController {
+}
